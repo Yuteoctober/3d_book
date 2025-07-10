67cac0e3daf286ec988f77b9aa5e822a6de2e3f5
Yute Lilitprapun <yudthsoponvit@gmail.com>
2025-07-10 08:29:22 -0400

    .

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -48,7 +48,7 @@ export const UI = () => {
   const [page, setPage] = useAtom(pageAtom);
 
   useEffect(() => {
-    const audio = new Audio("/audios/page-flip-01a.mp3");
+    const audio = new Audio("audios/page-flip-01a.mp3");
     audio.play();
   }, [page]);
 
